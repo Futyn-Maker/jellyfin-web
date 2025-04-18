1d074e833948605f634db39c794e6d41bc6a2090
Futyn-Maker <github@yakuboy.ru>
2025-04-18 11:12:29 +0300

    restore the visibility of the link to the card for the screen reader and assign it the role of an image

--- src/components/cardbuilder/cardBuilder.js
+++ src/components/cardbuilder/cardBuilder.js
@@ -1015,7 +1015,7 @@ function buildCard(index, item, apiClient, options) {
 
         const url = appRouter.getRouteUrl(item);
         // Don't use the IMG tag with safari because it puts a white border around it
-        cardImageContainerOpen = imgUrl ? ('<a href="' + url + '" data-action="' + action + '" class="' + cardImageContainerClasses + ' ' + cardContentClass + ' itemAction lazy" data-src="' + imgUrl + '" ' + blurhashAttrib + cardImageContainerAriaLabelAttribute + ' aria-hidden="true" tabindex="-1">') : ('<a href="' + url + '" data-action="' + action + '" class="' + cardImageContainerClasses + ' ' + cardContentClass + ' itemAction"  aria-hidden="true" tabindex="-1">');
+        cardImageContainerOpen = imgUrl ? ('<a href="' + url + '" data-action="' + action + '" class="' + cardImageContainerClasses + ' ' + cardContentClass + ' itemAction lazy" data-src="' + imgUrl + '" ' + blurhashAttrib + cardImageContainerAriaLabelAttribute + ' role="image" tabindex="-1">') : ('<a href="' + url + '" data-action="' + action + '" class="' + cardImageContainerClasses + ' ' + cardContentClass + ' itemAction"' + cardImageContainerAriaLabelAttribute + 'role="image" tabindex="-1">');
 
         cardImageContainerClose = '</a>';
     }
